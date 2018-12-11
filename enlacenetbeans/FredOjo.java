cdbce07352e9b218a5e3bb265fb89a8d269fb8be
fojomars <fojomars@victoria130.danielcastelao.org>
2018-12-11 09:24:52 +0100

    Nueclase

---
+++ enlacenetbeans/FredOjo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enlacenetbeans;
+
+/**
+ *
+ * @author fojomars
+ */
+public class FredOjo {
+    public void sumar(){
+        int num1 = 5,num2 =6;
+        System.out.println(num1+num2);
+    }
+}
